2458793c6d531fa94b44e2cafb630d23d7d87ab1
あずき⪥™ <44765629+melt-adzuki@users.noreply.github.com>
2025-07-25 17:48:00 +0900

    refactor `EventHandler`

--- neoforge/src/main/kotlin/me/adzuki/activitylogger/neoforge/EventHandler.kt
+++ neoforge/src/main/kotlin/me/adzuki/activitylogger/neoforge/EventHandler.kt
@@ -5,8 +5,8 @@ import me.adzuki.activitylogger.core.EventLogger
 import me.adzuki.activitylogger.core.PlayerInfoLogger
 import me.adzuki.activitylogger.core.PlayerPosition
 import me.adzuki.activitylogger.core.StatsLogger
+import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.phys.Vec3
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.neoforge.event.ServerChatEvent
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent
@@ -17,7 +17,7 @@ import org.slf4j.Logger
 
 internal object EventHandler {
     private val logger: Logger = LogUtils.getLogger()
-    private val Vec3.playerPosition get() = PlayerPosition(x, y, z)
+    private val Player.position get() = PlayerPosition(x, y, z)
 
     @SubscribeEvent
     fun onServerStarting(event: ServerStartingEvent) {
@@ -32,43 +32,46 @@ internal object EventHandler {
 
     @SubscribeEvent
     fun onPlayerJoin(event: PlayerEvent.PlayerLoggedInEvent) {
-        val player = event.entity
+        val player = event.entity as? ServerPlayer ?: return
         val playerName = player.name.string
-        val server = player.server!!
+        val server = player.server
 
         EventLogger(
             eventName = "Join",
             playerName = playerName,
-            position = player.position().playerPosition,
+            position = player.position,
         ).write()
         StatsLogger(playerCount = server.playerCount).write()
         PlayerInfoLogger(
             playerName = playerName,
-            playerIP = server.playerList.getPlayer(player.uuid)!!.ipAddress,
+            playerIP = player.ipAddress,
             playerUUID = player.uuid,
         ).write()
     }
 
     @SubscribeEvent
     fun onPlayerLeave(event: PlayerEvent.PlayerLoggedOutEvent) {
-        val player = event.entity
-        val server = player.server!!
+        val player = event.entity as? ServerPlayer ?: return
+        val server = player.server
 
         EventLogger(
             eventName = "Leave",
             playerName = player.name.string,
-            position = player.position().playerPosition,
+            position = player.position,
         ).write()
         StatsLogger(playerCount = server.playerCount - 1).write()
     }
 
     @SubscribeEvent
     fun onPlayerChat(event: ServerChatEvent) {
+        val player = event.player
+        val chatMessage = event.message.string
+
         EventLogger(
             eventName = "Chat",
-            playerName = event.player.name.string,
-            content = event.message.string,
-            position = event.player.position().playerPosition,
+            playerName = player.name.string,
+            content = chatMessage,
+            position = player.position,
         ).write()
     }
 
@@ -81,7 +84,7 @@ internal object EventHandler {
             eventName = "Death",
             playerName = player.name.string,
             content = deathMessage,
-            position = player.position().playerPosition,
+            position = player.position,
         ).write()
     }
 }
